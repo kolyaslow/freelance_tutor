50df079e07b36091df73c943c9e04c3da265d7ff
79146 <pysenkon@gmail.com>
2023-11-03 10:34:47 +0700

    create model User

---
+++ api_v1/auth/models.py
@@ -0,0 +1,29 @@
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import String, Boolean
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+from core import db_helper
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    email: Mapped[str] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String(length=1024), nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+
+
+async def get_user_db(session: AsyncSession = Depends(db_helper.session_dependency)):
+    yield SQLAlchemyUserDatabase(session, User)
