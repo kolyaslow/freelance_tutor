6c38c56b7da7e318977630778a37324bc1212d55
79146 <pysenkon@gmail.com>
2023-11-04 11:51:01 +0700

    content transfer from auth_backend.py in config.py

--- api_v1/user/config.py
+++ api_v1/user/config.py
@@ -1,7 +1,24 @@
 import os
 from dotenv import load_dotenv
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
 
 load_dotenv()
 
 SECRET_KEY_BY_JWT = os.environ.get('SECRET_KEY_BY_JWT')
 SECRET_KEY_BY_UserManager = os.environ.get('SECRET_KEY_BY_UserManager')
+
+
+
+# Setings Authentication backends
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET_KEY_BY_JWT, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
