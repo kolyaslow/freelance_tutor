cc8b5bfcfe1644edb20622bfba4a8e8a2703d365
79146 <pysenkon@gmail.com>
2023-11-05 14:02:30 +0700

    The fields is_active, is_verified, oauth_accounts have been removedfrom the input field in the CreateUser schema API

--- api_v1/user/schemas.py
+++ api_v1/user/schemas.py
@@ -1,12 +1,15 @@
 from fastapi_users import schemas
+from pydantic import EmailStr
 
 
 class UserRead(schemas.BaseUser[int]):
     pass
 
 
-class UserCreate(schemas.BaseUserCreate):
-    pass
+class UserCreate(schemas.CreateUpdateDictModel):
+    email: EmailStr
+    password: str
+
 
 
 class UserUpdate(schemas.BaseUserUpdate):
--- core/models/user.py
+++ core/models/user.py
@@ -18,7 +18,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     hashed_password: Mapped[str] = mapped_column(
         String(length=1024), nullable=False
     )
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(
         Boolean, default=False, nullable=False
     )
