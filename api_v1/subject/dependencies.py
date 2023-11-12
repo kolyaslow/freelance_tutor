17bdd4968fa4aa1a329e6b217bc741e9a95f0721
79146 <pysenkon@gmail.com>
2023-11-12 18:28:11 +0700

    creating a dependency to get an item by its name

---
+++ api_v1/subject/dependencies.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import Path, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Subject
+from core.db_helper import db_helper
+from . import crud
+
+async def get_subject_by_name(
+    subject_name: Annotated[str, Path],
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> Subject:
+    subject = await crud.get_subject(subject_name=subject_name, session=session)
+    if subject:
+        return subject
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {subject_name} not found!",
+    )
