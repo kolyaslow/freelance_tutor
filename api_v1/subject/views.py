eb1313b5855cb7a5afee8dbc068e90595df9f5e0
79146 <pysenkon@gmail.com>
2023-11-12 20:27:36 +0700

    update depend by check aut user

--- api_v1/subject/views.py
+++ api_v1/subject/views.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.db_helper import db_helper
-from ..user import fastapi_users
+from ..user import current_user
 from . import crud
 from .schemas import CreateSubject
 from core.models import Subject
@@ -15,15 +15,16 @@ from .dependencies import get_subject_by_name
 router = APIRouter(tags=['Subject'])
 
 
-@router.post('/create_subject', dependencies=[Depends(fastapi_users.current_user(optional=True, active=True,  superuser=True))])
+@router.post('/create_subject', dependencies=[Depends(current_user.superuser)])
 async def create_subject(
         subject_in: CreateSubject,
         session: AsyncSession = Depends(db_helper.session_dependency),
+
 ) -> JSONResponse:
     return await crud.create_subject(session, subject_in)
 
 
-@router.delete('/delete_subject/{subject_name}', dependencies=[Depends(fastapi_users.current_user(optional=True, active=True,  superuser=True))])
+@router.delete('/delete_subject/{subject_name}', dependencies=[Depends(current_user.superuser)])
 async def delete_subject(
         subject: Subject = Depends(get_subject_by_name),
         session: AsyncSession = Depends(db_helper.session_dependency),
