69476e48c9da09b5848d6fc2ba524678fa525cef
79146 <pysenkon@gmail.com>
2024-01-23 11:24:29 +0700

    Перенес router для аутентификации в __init__ файл api_v1

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,11 +1,18 @@
-from .user.views import router as user_router
+from .user.config import auth_backend
+from .user.schemas import UserRead, UserCreate
+from .user.views import router as user_router, fastapi_users
 from fastapi import APIRouter
 from .subject.views import router as subject_router
 from .profile.views import router as profile_router
 
 router = APIRouter()
 
-router.include_router(router=user_router)
+router.include_router(
+    router=user_router,
+    prefix='/user',
+    tags=['User']
+)
+
 router.include_router(
     router=subject_router,
     prefix='/subject',
@@ -18,3 +25,14 @@ router.include_router(
     tags=['Profile'],
 )
 
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
