3d085c49d84c97eb0e59773f5675e4b06bc97a59
79146 <pysenkon@gmail.com>
2024-01-24 13:44:34 +0700

    Добил тесты для router на получения пердметов, которые ведет репетитор

---
+++ tests/user/test_router_get_subject.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from httpx import Response
+from fastapi import status
+
+from tests.conftest import BaseRequestAPI
+
+class TestGetSubject(BaseRequestAPI):
+
+    _url = '/user/get_subject'
+    _method = 'get'
+
+    async def request_by_api(self, headers: dict[str, Any] = None) -> Response:
+        return await super().request_by_api(headers=headers)
+
+
+    async def test_by_tutor(self, auth_headers_tutor, add_subject_by_tutor):
+        """Проверка получения репетитором, предметов, которые он ведет"""
+        response = await self.request_by_api(headers=auth_headers_tutor)
+        assert response.status_code == status.HTTP_200_OK
