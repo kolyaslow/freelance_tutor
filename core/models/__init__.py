f699ef6ce74a2f860eaaa4055e63818cf52c9d14
79146 <pysenkon@gmail.com>
2024-01-29 18:48:17 +0700

    Создание таблицы заказы.

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,12 +1,6 @@
-__all__ = [
-    'Base',
-    'User',
-    'Subject',
-    'Profile'
-]
-
 from .base import Base
 from .user import User
 from .subject import Subject
 from .subject_user_association import SubjectUserAssociation
 from .profile import Profile
+from .order import Order
---
+++ core/models/order.py
@@ -0,0 +1,28 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, UniqueConstraint
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Order(Base):
+
+    __table_args__ = (
+        UniqueConstraint(
+            "user_id",
+            "subject_name",
+            name="idx_unique_user_subject_order",
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    subject_name: Mapped[int] = mapped_column(ForeignKey('subject.name'))
+    description: Mapped[str]
+    is_active: Mapped[bool] = mapped_column(default=True)
+
+    users: Mapped[list[User]] = relationship('User')
