2eb0b9a0d21bbeb59169a22591770197d44a5100
79146 <pysenkon@gmail.com>
2024-01-29 17:35:14 +0700

    Разбил конфигурацию на файлы

---
+++ tests/common/__init__.py
@@ -0,0 +1,17 @@
+from .user_authentication_fixture import (
+    user_customer_data,
+    user_tutor_data,
+    register_tutor,
+    register_customer,
+    auth_headers_tutor,
+    auth_headers_customer,
+    auth_customer,
+    auth_tutor,
+)
+from .fixture_profile_management import (
+    get_profile,
+    create_profile_by_tutor,
+    delete_profile,
+)
+from .base_request_api import BaseRequestAPI
+
