3ae8a629efe309fd13b97fe5738d0ae10a40ae89
79146 <pysenkon@gmail.com>
2024-02-04 11:18:50 +0700

    Изменил зависмость, используя общую: get_item_db_by_id

--- api_v1/subject/dependencies.py
+++ api_v1/subject/dependencies.py
@@ -5,17 +5,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Subject
 from core.db_helper import db_helper
-from . import crud
 from .schemas import AllowedValuesByName
+from ..common.dependencies import get_item_db_by_id
 
 async def get_subject(
     subject_name: Annotated[AllowedValuesByName, Path],
     session: AsyncSession = Depends(db_helper.session_dependency),
 ) -> Subject:
-    subject = await crud.get_subject(subject_name=subject_name, session=session)
-    if subject:
-        return subject
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Product {subject_name} not found!",
+    subject = await get_item_db_by_id(
+        session=session,
+        item_id=subject_name,
+        model_db=Subject
     )
+    return subject
