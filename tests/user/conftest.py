eeef31b597a4359895f6b27e9f3cffe554591408
79146 <pysenkon@gmail.com>
2024-02-04 17:26:27 +0700

    Изменил получение сессии, теперь берется не через фикстуру(все тесты работают)

--- tests/user/conftest.py
+++ tests/user/conftest.py
@@ -3,23 +3,25 @@ import pytest
 from api_v1.subject import crud as crud_subject
 from api_v1.user import crud as crud_user
 from api_v1.subject.schemas import CreateSubject
+from core.db_helper import db_helper
 
 @pytest.fixture(autouse=True, scope='session')
-async def create_subject(session):
+async def create_subject():
     subject_in = CreateSubject(name='informatics')
-    await crud_subject.create_subject(session=session, subject_in=subject_in)
+    async with db_helper.session_factory() as session:
+        await crud_subject.create_subject(session=session, subject_in=subject_in)
 
 
 @pytest.mark.usefixtures('create_subject')
 @pytest.fixture(scope='session')
-async def add_subject_by_tutor(register_tutor, session):
+async def add_subject_by_tutor(register_tutor):
 
     subject_names = ['informatics']
     user_id = register_tutor['id']
-
-    await crud_user.add_subjects_by_user(
-        session=session,
-        subjects=subject_names,
-        user_id=user_id
-)
+    async with db_helper.session_factory() as session:
+        await crud_user.add_subjects_by_user(
+            session=session,
+            subjects=subject_names,
+            user_id=user_id
+        )
 
