7f12609d2affa6e819a5f06e7ee139c8d3820d95
79146 <pysenkon@gmail.com>
2024-02-04 16:37:39 +0700

    Заменил сессию, на фикстуру

--- tests/user/conftest.py
+++ tests/user/conftest.py
@@ -3,25 +3,23 @@ import pytest
 from api_v1.subject import crud as crud_subject
 from api_v1.user import crud as crud_user
 from api_v1.subject.schemas import CreateSubject
-from core import db_helper
 
 @pytest.fixture(autouse=True, scope='session')
-async def create_subject():
+async def create_subject(session):
     subject_in = CreateSubject(name='informatics')
-
-    async with db_helper.session_factory() as session:
-        await crud_subject.create_subject(session=session, subject_in=subject_in)
+    await crud_subject.create_subject(session=session, subject_in=subject_in)
 
 
 @pytest.mark.usefixtures('create_subject')
 @pytest.fixture(scope='session')
-async def add_subject_by_tutor(register_tutor):
+async def add_subject_by_tutor(register_tutor, session):
+
     subject_names = ['informatics']
     user_id = register_tutor['id']
-    async with db_helper.session_factory() as session:
-        await crud_user.add_subjects_by_user(
-            session=session,
-            subjects=subject_names,
-            user_id=user_id
-        )
+
+    await crud_user.add_subjects_by_user(
+        session=session,
+        subjects=subject_names,
+        user_id=user_id
+)
 
