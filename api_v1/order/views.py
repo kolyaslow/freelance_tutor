511854b5b853d0ab465f5b1ba144af076772b436
79146 <pysenkon@gmail.com>
2024-02-05 19:17:30 +0700

    Добавил статус ответа 201

--- api_v1/order/views.py
+++ api_v1/order/views.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends, APIRouter, status
+from sqlalchemy.exc import IntegrityError
+from fastapi import Depends, APIRouter, status, HTTPException
 
 from core.db_helper import db_helper
 from .schemas import CreateOrder
@@ -15,18 +15,20 @@ router = APIRouter()
 @router.post(
         '/create_order',
         response_model=CreateOrder,
+        status_code=status.HTTP_201_CREATED
 )
 async def create_order(
         order_in: CreateOrder,
         session: AsyncSession = Depends(db_helper.session_dependency),
         user: User = Depends(user_rights.checking_customer),
 ) -> Order:
-        order_in.user_id = user.id
-        return await crud_common.create_db_item(
-                session=session,
-                model_db=Order,
-                data=order_in,
-        )
+    order_in.user_id = user.id
+
+    return await crud_common.create_db_item(
+            session=session,
+            model_db=Order,
+            data=order_in,
+    )
 
 
 @router.post(
@@ -38,7 +40,7 @@ async def delete_order(
         order: Order = Depends(get_order_by_id),
         session: AsyncSession = Depends(db_helper.session_dependency)
 ):
-        await crud_common.delete_db_item(
-                session=session,
-                delete_item=order,
-        )
+    await crud_common.delete_db_item(
+        session=session,
+        delete_item=order,
+    )
