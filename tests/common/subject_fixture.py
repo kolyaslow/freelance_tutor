5d037e8b7fc8cf7e0f8aa7ad3432e7e5642eedc0
79146 <pysenkon@gmail.com>
2024-02-05 22:33:53 +0700

    Слелал возврат объекта subject в create_subject

--- tests/common/subject_fixture.py
+++ tests/common/subject_fixture.py
@@ -3,11 +3,13 @@ import pytest
 from api_v1.subject import crud as crud_subject
 from api_v1.subject.schemas import CreateSubject
 from core import db_helper
+from core.models import Subject
 
 
 @pytest.mark.usefixtures('prepare_database')
 @pytest.fixture(scope='session')
-async def create_subject():
+async def create_subject() -> Subject:
     subject_in = CreateSubject(name='informatics')
     async with db_helper.session_factory() as session:
-        await crud_subject.create_subject(session=session, subject_in=subject_in)
+        subject = await crud_subject.create_subject(session=session, subject_in=subject_in)
+        return subject
