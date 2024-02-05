9c570c9500041e32b66751cd74bbdb96cce68c0e
79146 <pysenkon@gmail.com>
2024-02-05 22:32:41 +0700

    Исправил получения парметра order_id

--- api_v1/order/dependencies.py
+++ api_v1/order/dependencies.py
@@ -1,14 +1,15 @@
+from typing import Annotated
+
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, Path
 
 from core.db_helper import db_helper
 from core.models import Order
-from ..common import crud as crud_common
 from ..common.dependencies import get_item_db_by_id
 
 
 async def get_order_by_id(
-        order_id: int,
+        order_id: Annotated[int, Path],
         session: AsyncSession = Depends(db_helper.session_dependency)
 ) -> Order:
     return await get_item_db_by_id(
