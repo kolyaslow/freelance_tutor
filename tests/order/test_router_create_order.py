f799ad435b83a3476ce335b28b6b88f54375d156
79146 <pysenkon@gmail.com>
2024-02-05 19:15:44 +0700

    Добавил тесты для Api по созданию заказа

--- tests/order/test_router_create_order.py
+++ tests/order/test_router_create_order.py
@@ -1,14 +1,57 @@
 from tests.conftest import BaseRequestAPI
+from fastapi import status
+import pytest
 
 
-class CreateOrder(BaseRequestAPI):
+class TestCreateOrder(BaseRequestAPI):
 
-    _url = 'oreder/cretae_order'
+    _url = '/order/create_order'
     _method = 'post'
-    _data = {
-        'description': None,
-        'is_active': True,
+    _json = {
+        'description': 'Первый заказ',
+        'is_activ': None,
         'subject_name': 'informatics',
     }
+
+    @pytest.mark.parametrize(
+        'description, is_active, status',
+        [
+            ('Первый заказ', True, status.HTTP_201_CREATED,),
+            ('Первый заказ', False, status.HTTP_201_CREATED,),
+            (None, True, status.HTTP_422_UNPROCESSABLE_ENTITY),
+            (None, False, status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ]
+    )
+    async def test_by_customer(
+        self,
+        auth_headers_customer,
+        create_subject,
+        description,
+        is_active,
+        status,
+    ):
+        """Проверка на доступность APi на создание заказа для role=customer"""
+
+        self._json['description'] = description
+        self._json['is_active'] = is_active
+        request = await self.request_by_api(
+            headers=auth_headers_customer
+        )
+        assert request.status_code == status
+
+
+    async def test_recreating_order(
+        self,
+        create_order,
+        auth_headers_customer,
+        create_subject,
+    ):
+        """Проверка на успешное создание повторного заказа"""
+        request = await self.request_by_api(
+            headers=auth_headers_customer
+        )
+        assert request.status_code == status.HTTP_201_CREATED
+
+
 
     
