6c4518ef0769577e3010fcae2e035101a16faa7e
79146 <pysenkon@gmail.com>
2024-02-05 19:17:02 +0700

    Сделал поле description для заказа енобязательным

--- api_v1/order/schemas.py
+++ api_v1/order/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from api_v1.subject.schemas import AllowedValuesByName
 
 class BaseOrder(BaseModel):
-    description: str | None = None
+    description: str
     is_active: bool | None = True
     subject_name: AllowedValuesByName
     user_id: int | None = None
