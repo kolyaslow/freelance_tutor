7e4d95b50a677e51cf2f8ec99450d6d50c461d8e
79146 <pysenkon@gmail.com>
2024-02-06 12:37:33 +0700

    Разбил один тест, на несколько

--- tests/order/test_router_create_order.py
+++ tests/order/test_router_create_order.py
@@ -6,38 +6,41 @@ from core.models import Order
 
 
 class TestCreateOrder(BaseRequestAPI):
-
+    """Првоерка работы API, для пользователя с role=customer"""
     _url = '/order/create_order'
     _method = 'post'
-    _json = {
-        'subject_name': 'informatics',
-    }
+    _json = None
 
-    @pytest.mark.parametrize(
-        'description, is_active, status',
-        [
-            ('Первый заказ', True, status.HTTP_201_CREATED,),
-            ('Первый заказ', False, status.HTTP_201_CREATED,),
-            (None, True, status.HTTP_422_UNPROCESSABLE_ENTITY),
-            (None, False, status.HTTP_422_UNPROCESSABLE_ENTITY),
-        ]
-    )
-    async def test_by_customer(
+    async def test_missing_subject_name(self, auth_headers_customer, order: Order):
+        self._json = {
+            'description': order.description,
+            'is_active': order.is_active,
+        }
+        request = await self.request_by_api(headers=auth_headers_customer)
+        assert request.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    async def test_missing_description(self, auth_headers_customer, order: Order):
+        self._json = {
+            'subject_name': order.subject_name,
+            'is_active': order.is_active,
+        }
+        request = await self.request_by_api(headers=auth_headers_customer)
+        assert request.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    async def test_valid_data(
         self,
         auth_headers_customer,
         create_subject,
-        description,
-        is_active,
-        status,
+        order: Order,
     ):
-        """Проверка на доступность APi на создание заказа для role=customer"""
+        self._json = {
+            'description': order.description,
+            'is_active': order.is_active,
+            'subject_name': order.subject_name,
+        }
+        request = await self.request_by_api(headers=auth_headers_customer)
+        assert request.status_code == status.HTTP_201_CREATED
 
-        self._json['description'] = description
-        self._json['is_active'] = is_active
-        request = await self.request_by_api(
-            headers=auth_headers_customer
-        )
-        assert request.status_code == status
 
 
     async def test_recreating_order(
@@ -47,7 +50,7 @@ class TestCreateOrder(BaseRequestAPI):
         create_subject,
         order: Order,
     ):
-        """Проверка на успешное создание повторного заказа"""
+        """Проверка создания дубликата заказа"""
         self._json = {
             'description': order.description,
             'is_activ': order.is_active,
